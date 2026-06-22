f4fcd6289f9a350c90e18f663845655c9f03674a
SlimeNull <slimenull@outlook.com>
2026-06-22 23:38:24 +0800

    feat: 开始添加普通凋落物的高亮

--- source/DuckovCoreUtilities/Features/LootboxOutlineFeature.cs
+++ source/DuckovCoreUtilities/Features/LootboxOutlineFeature.cs
@@ -34,6 +34,77 @@ namespace SlimeNull.DuckovCoreUtilities.Features
             var outline = instance.GetOrAddComponent<LootOutlinable>();
         }
 
+        private static void EnsureOutlineAttached(InteractablePickup instance)
+        {
+            var outline = instance.GetOrAddComponent<GroundItemOutlinable>();
+        }
+
+        private abstract class OutlinableBehaviourBase : MonoBehaviour
+        {
+            protected Outlinable? Outlinable { get; set; }
+
+            private CharacterMainControl? _player;
+
+            public int ActivationDistance { get; set; } = 10;
+
+            protected virtual void Update()
+            {
+                if (Outlinable is null)
+                {
+                    return;
+                }
+
+                _player ??= LevelManager.Instance.MainCharacter;
+                if (_player is null)
+                {
+                    return;
+                }
+
+                Outlinable.enabled = Vector3.Distance(transform.position, _player.transform.position) < ActivationDistance;
+            }
+
+            protected static void ConfigureOutline(Outlinable outlinable, Color color)
+            {
+                outlinable.OutlineParameters.Enabled = true;
+                outlinable.OutlineParameters.Color = color;
+                outlinable.OutlineParameters.DilateShift = 3f;
+                outlinable.enabled = false;
+            }
+
+            protected static bool ShouldOutlineRenderer(Renderer renderer)
+            {
+                if (renderer == null ||
+                    !renderer.enabled ||
+                    renderer.GetComponentInParent<InteractMarker>() != null ||
+                    renderer.GetComponentInParent<Canvas>() != null ||
+                    renderer.GetComponent<SodaPointLight>() != null ||
+                    renderer.name == "SodaPointLight")
+                {
+                    return false;
+                }
+
+                if (renderer is LineRenderer ||
+                    renderer is TrailRenderer ||
+                    renderer is ParticleSystemRenderer)
+                {
+                    return false;
+                }
+
+                if (renderer is MeshRenderer)
+                {
+                    var meshFilter = renderer.GetComponent<MeshFilter>();
+                    return meshFilter != null && meshFilter.sharedMesh != null;
+                }
+
+                if (renderer is SkinnedMeshRenderer skinnedMeshRenderer)
+                {
+                    return skinnedMeshRenderer.sharedMesh != null;
+                }
+
+                return false;
+            }
+        }
+
         private class LootOutlinable : MonoBehaviour
         {
             private InteractableLootbox? lootBox;
@@ -175,13 +246,80 @@ namespace SlimeNull.DuckovCoreUtilities.Features
             }
         }
 
-        [HarmonyPatchCategory(HarmonyCatagory)]
-        [HarmonyPatch(typeof(InteractableLootbox), "Start")]
-        private static class LootBoxStartPatch
+        private class GroundItemOutlinable : OutlinableBehaviourBase
         {
-            private static void Postfix(InteractableLootbox __instance)
+            private InteractablePickup? _pickup;
+
+            private void Start()
             {
-                EnsureOutlineAttached(__instance);
+                _pickup = GetComponent<InteractablePickup>();
+                if (ShouldApplyOutline(_pickup))
+                {
+                    AttachOutline();
+                }
+            }
+
+            private void AttachOutline()
+            {
+                if (_pickup is null)
+                {
+                    return;
+                }
+
+                var spriteRenderer = _pickup.GetComponentInChildren<SpriteRenderer>();
+                if (spriteRenderer != null)
+                {
+                    Outlinable = spriteRenderer.gameObject.GetComponent<Outlinable>() ??
+                        spriteRenderer.gameObject.AddComponent<Outlinable>();
+                    Outlinable.AddRenderer(spriteRenderer);
+                    ConfigureOutline(Outlinable, Color.white);
+                    return;
+                }
+
+                Outlinable = gameObject.GetComponent<Outlinable>() ?? gameObject.AddComponent<Outlinable>();
+                foreach (var renderer in GetComponentsInChildren<Renderer>(false))
+                {
+                    if (ShouldOutlineRenderer(renderer) &&
+                        renderer.name != "Quad")
+                    {
+                        Outlinable.AddRenderer(renderer);
+                    }
+                }
+
+                if (Outlinable.OutlineTargetsCount > 0)
+                {
+                    ConfigureOutline(Outlinable, Color.white);
+                }
+                else
+                {
+                    Destroy(Outlinable);
+                    Outlinable = null;
+                }
+            }
+
+            private static bool ShouldApplyOutline(InteractablePickup? pickup)
+            {
+                return pickup != null &&
+                    pickup.isActiveAndEnabled &&
+                    pickup.ItemAgent != null &&
+                    pickup.ItemAgent.Item != null;
+            }
+        }
+
+        [HarmonyPatchCategory(HarmonyCatagory)]
+        [HarmonyPatch(typeof(InteractableBase), "Start")]
+        private static class InteractableBaseStartPatch
+        {
+            private static void Postfix(InteractableBase __instance)
+            {
+                if (__instance is InteractableLootbox lootbox)
+                {
+                    EnsureOutlineAttached(lootbox);
+                }
+                else if (__instance is InteractablePickup pickup)
+                {
+                    EnsureOutlineAttached(pickup);
+                }
             }
         }
     }
