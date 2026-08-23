f04d59805ea574e4bd8dc87370f0827400437407
SlimeNull <slimenull@outlook.com>
2026-08-23 12:16:07 +0800

    chore: 整理代码, 一些注释

--- source/DuckovCoreUtilities/ModBehaviour.cs
+++ source/DuckovCoreUtilities/ModBehaviour.cs
@@ -16,24 +16,19 @@ namespace SlimeNull.DuckovCoreUtilities
                 return;
             }
 
-            Task.Run(() =>
-            {
-                Debug.Log("In task action");
-            });
-
             Debug.Log("loading DCU");
 
             _features = new FeatureHost(gameObject);
-            _features.Register(new DisplayPriceFeature());
-            _features.Register(new DisplayStorageCount());
-            _features.Register(new DisplayQualityFeature());
-            _features.Register(new LootboxOutlineFeature());
-            _features.Register(new InventorySortButtonsFeature());
-            _features.Register(new AutoCloseBackpackFeature());
-            _features.Register(new AutoFadeHudWhenAimingFeature());
-            _features.Register(new BulletCountCrosshairColorFeature());
-            _features.Register(new LowHealthInnerShadowFeature());
-            _features.Register(new KillRecordFeature());
+            _features.Register(new DisplayPriceFeature());                     // 显示价值
+            _features.Register(new DisplayStorageCount());                     // 显示仓库物品数量
+            _features.Register(new DisplayQualityFeature());                   // 显示物品品质
+            _features.Register(new LootboxOutlineFeature());                   // 显示战利品箱轮廓
+            _features.Register(new InventorySortButtonsFeature());             // 显示仓库排序按钮
+            _features.Register(new AutoCloseBackpackFeature());                // 自动关闭背包
+            _features.Register(new AutoFadeHudWhenAimingFeature());            // 瞄准时 HUD 淡出
+            _features.Register(new BulletCountCrosshairColorFeature());        // 弹药数量准心颜色
+            _features.Register(new LowHealthInnerShadowFeature());             // 低血量内阴影
+            _features.Register(new KillRecordFeature());                       // 击杀记录
             _features.EnableAll();
 
             Debug.Log("loaded DCU");
