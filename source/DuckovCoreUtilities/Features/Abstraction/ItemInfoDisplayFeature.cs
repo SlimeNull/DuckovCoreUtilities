819bbe4cbeb3c155459e24c7da016d4e6074c050
SlimeNull <slimenull@outlook.com>
2026-08-27 12:58:38 +0800

    feat: 物品使用情况也同步到 ItemInfoDisplayFeature 继承

--- source/DuckovCoreUtilities/Features/Abstraction/ItemInfoDisplayFeature.cs
+++ source/DuckovCoreUtilities/Features/Abstraction/ItemInfoDisplayFeature.cs
@@ -9,7 +9,7 @@ namespace SlimeNull.DuckovCoreUtilities.Features.Abstraction
 {
     public abstract class ItemInfoDisplayFeature : FeatureBase
     {
-        private TextMeshProUGUI? _text = null;
+        private TextMeshProUGUI? _text;
 
         protected override void OnEnable()
         {
@@ -21,18 +21,24 @@ namespace SlimeNull.DuckovCoreUtilities.Features.Abstraction
         {
             ItemHoveringUI.onSetupItem -= OnSetupItemHoveringUI;
             ItemHoveringUI.onSetupMeta -= OnSetupMeta;
+
+            if (_text != null)
+            {
+                UnityEngine.Object.Destroy(_text.gameObject);
+                _text = null;
+            }
         }
 
-        private void OnSetupMeta(ItemHoveringUI uI, ItemMetaData data)
+        private void OnSetupMeta(ItemHoveringUI _, ItemMetaData __)
         {
-            _text?.gameObject.SetActive(false);
+            SetVisible(false);
         }
 
         private void OnSetupItemHoveringUI(ItemHoveringUI uiInstance, Item item)
         {
-            if (item == null)
+            if (item == null || !ShouldDisplay(uiInstance, item))
             {
-                _text?.gameObject?.SetActive(false);
+                SetVisible(false);
                 return;
             }
 
@@ -44,12 +50,21 @@ namespace SlimeNull.DuckovCoreUtilities.Features.Abstraction
             }
 
             _text.gameObject.SetActive(true);
-            _text.transform.SetParent(uiInstance.LayoutParent);
+            _text.transform.SetParent(uiInstance.LayoutParent, false);
             _text.transform.localScale = Vector3.one;
             _text.text = textToDisplay;
             _text.fontSize = 20f;
         }
 
+        private void SetVisible(bool visible)
+        {
+            if (_text != null)
+            {
+                _text.gameObject.SetActive(visible);
+            }
+        }
+
+        protected virtual bool ShouldDisplay(ItemHoveringUI uiInstance, Item item) => true;
 
         protected abstract string GetText(ItemHoveringUI uiInstance, Item item);
     }
--- source/DuckovCoreUtilities/Features/ItemUsageDisplayFeature.cs
+++ source/DuckovCoreUtilities/Features/ItemUsageDisplayFeature.cs
@@ -1,75 +1,44 @@
 using Duckov.UI;
-using Duckov.Utilities;
 using ItemStatsSystem;
-using SlimeNull.DuckovCoreUtilities.Infrastructure;
+using SlimeNull.DuckovCoreUtilities.Features.Abstraction;
 using SodaCraft.Localizations;
-using TMPro;
 using UnityEngine;
 
 namespace SlimeNull.DuckovCoreUtilities.Features
 {
-    internal sealed class ItemUsageDisplayFeature : FeatureBase
+    internal sealed class ItemUsageDisplayFeature : ItemInfoDisplayFeature
     {
         private const string UsageLocalizationKey = "SlimeNull.DuckovCoreUtilities.ItemUsage.Available";
-        private TextMeshProUGUI? _text;
 
         public override string Name => "Item usage display";
 
         protected override void OnEnable()
         {
-            ItemHoveringUI.onSetupItem += OnSetupItem;
-            ItemHoveringUI.onSetupMeta += OnSetupMeta;
+            base.OnEnable();
+
             LocalizationManager.OnSetLanguage += UpdateLocalization;
             UpdateLocalization(LocalizationManager.CurrentLanguage);
         }
 
         protected override void OnDisable()
         {
-            ItemHoveringUI.onSetupItem -= OnSetupItem;
-            ItemHoveringUI.onSetupMeta -= OnSetupMeta;
+            base.OnDisable();
+
             LocalizationManager.OnSetLanguage -= UpdateLocalization;
             LocalizationManager.RemoveOverrideText(UsageLocalizationKey);
-            if (_text != null)
-            {
-                UnityEngine.Object.Destroy(_text.gameObject);
-                _text = null;
-            }
         }
 
-        private void OnSetupMeta(ItemHoveringUI _, ItemMetaData __)
+        protected override bool ShouldDisplay(ItemHoveringUI uiInstance, Item item)
         {
-            SetVisible(false);
+            return item.UseDurability;
         }
 
-        private void OnSetupItem(ItemHoveringUI ui, Item item)
+        protected override string GetText(ItemHoveringUI uiInstance, Item item)
         {
-            if (item == null || !item.UseDurability)
-            {
-                SetVisible(false);
-                return;
-            }
-
-            if (_text == null)
-            {
-                _text = UnityEngine.Object.Instantiate(GameplayDataSettings.UIStyle.TemplateTextUGUI);
-            }
-
-            _text.gameObject.SetActive(true);
-            _text.transform.SetParent(ui.LayoutParent, false);
-            _text.transform.localScale = Vector3.one;
             var label = LocalizationManager.GetPlainText(UsageLocalizationKey);
-            _text.text = item.DurabilityLoss > 0.000001f
+            return item.DurabilityLoss > 0.000001f
                 ? $"{label}: {(int)item.Durability}/{(int)item.MaxDurability} ({(int)item.MaxDurabilityWithLoss})"
                 : $"{label}: {(int)item.Durability}/{(int)item.MaxDurability}";
-            _text.fontSize = 20f;
-        }
-
-        private void SetVisible(bool visible)
-        {
-            if (_text != null)
-            {
-                _text.gameObject.SetActive(visible);
-            }
         }
 
         private static void UpdateLocalization(SystemLanguage language)
